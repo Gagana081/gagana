1a6bbde568c7033d63422535df0127e5b486e5cb
GAGANA S <sonugagana12@gmail.com>
2025-06-17 11:17:53 +0530

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -5,10 +5,46 @@ from photoapp.models import Photo, Folder
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password_confirm = serializers.CharField(write_only=True)
+    
     class Meta:
         model = User
-        fields = ('id', 'username', 'email') 
+        fields = ('id', 'username', 'email', 'password', 'password_confirm')
         read_only_fields = ('id',)
+    
+    def validate(self, attrs):
+        # Check if passwords match
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError("Passwords don't match")
+        return attrs
+    
+    def create(self, validated_data):
+        # Remove password_confirm from validated_data since it's not a model field
+        validated_data.pop('password_confirm', None)
+        
+        # Create user with properly hashed password using create_user method
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        return user
+    
+    def update(self, instance, validated_data):
+        # Handle password update if provided
+        password = validated_data.pop('password', None)
+        validated_data.pop('password_confirm', None)
+        
+        if password:
+            instance.set_password(password)
+            
+        # Update other fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+            
+        instance.save()
+        return instance
 
 class FolderSerializer(serializers.ModelSerializer):
     class Meta:
